60db028a174b83fe99ae17d2dfb77d673a20ac45
Mayank Sharma <shar.mayankofficial@gmail.com>
2025-10-20 15:46:04 +0530

    enhanced the modularity of the esp firmware

---
+++ firmware/ESP32S3_LoRa/lib/BlinkTest/src/BlinkTest.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Arduino.h>
+
+// Free functions API
+void BlinkTest_setup();
+void BlinkTest_loop();
---
+++ firmware/ESP32S3_LoRa/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include "BlinkTest.h"
+
+void setup() {
+    BlinkTest_setup();
+}
+
+void loop() {
+    BlinkTest_loop();
+}
